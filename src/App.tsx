2f5e3499a9b81ea18f15cd09733ab70b3ac7bd20
Berdinazar <slybrd01@gmail.com>
2025-09-07 07:36:28 +0300

    feat: add shadcn for ui, handle form function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
-
+const [searchValue, setSearchValue] = useState("");
+const [query, setQuery] = useState("");
+const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setSearchValue(e.target.value);
+  
+}
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuery(searchValue);
+  }
   return (
     <>
-     <h2 className="text-4xl">1231232</h2>qweqw
+      <div className="p-4 max-w-3xl mx-auto">
+        <form onSubmit={onFormSubmit} className="flex relative">
+          <Input name="search" onChange={onSearchChange} value={searchValue} placeholder="Search movie..." />
+          <Button variant={"outline"} className="absolute right-0" type="submit">
+            Search
+          </Button>
+        </form>
+
+        <h3>{query}</h3>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
