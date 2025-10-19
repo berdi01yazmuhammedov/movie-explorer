906bc87cbe6a221e29e2537a04470bc842e39d3f
Berdinazar <slybrd01@gmail.com>
2025-10-19 08:58:04 +0300

    fix: move input to header

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,13 @@ import { useAppDispatch, useAppSelector } from './store/hooks';
 
 import { useEffect } from 'react';
 import Filters from './components/Filters';
-import Header from './components/Header';
 
 function App() {
     const dispatch = useAppDispatch();
     const { searchValue, loading } = useAppSelector((state) => state.movies);
     const movies = useAppSelector(selectFilteredMovies);
 
-    const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        dispatch(setSearchValue(e.target.value));
-    };
-    const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        dispatch(setQuery(searchValue));
-        dispatch(fetchMovies({ query: searchValue }));
-    };
+    
     useEffect(() => {
         if (!movies.length) {
             dispatch(fetchMovies({ query: '' }));
@@ -44,22 +36,7 @@ function App() {
     return (
         <div className="px-8 py-10">
             
-            <form onSubmit={onFormSubmit} className="flex items-center justify-center gap-2 mb-10">
-                <Input
-                    className="w-full max-w-2xl bg-zinc-800 text-gray-200 border border-zinc-700 rounded-lg focus:ring-2 focus:ring-indigo-500"
-                    name="search"
-                    onChange={onSearchChange}
-                    value={searchValue}
-                    placeholder="Search movie..."
-                />
-                <Button
-                    variant="outline"
-                    className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg px-6"
-                    type="submit"
-                >
-                    Search
-                </Button>
-            </form>
+           
 
             <div className="flex gap-8 items-start">
                 <aside className="w-[240px] shrink-0 bg-zinc-900 p-5 rounded-xl border border-zinc-800 shadow-lg">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { fetchMovies, setQuery, setSearchValue } from '@/store/movieSlice';
 
 const Header = () => {
-  return (
-    <div><h1>Homepage</h1></div>
-  )
-}
+    const dispatch = useAppDispatch();
+    const { searchValue } = useAppSelector((state) => state.movies);
+    const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(setSearchValue(e.target.value));
+    };
+    const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(setQuery(searchValue));
+        dispatch(fetchMovies({ query: searchValue }));
+    };
+    return (
+        <header>
+            <form onSubmit={onFormSubmit} className="flex items-center justify-center gap-2 mb-10">
+                <Input
+                    className="w-full max-w-2xl bg-zinc-800 text-gray-200 border border-zinc-700 rounded-lg focus:ring-2 focus:ring-indigo-500"
+                    name="search"
+                    onChange={onSearchChange}
+                    value={searchValue}
+                    placeholder="Search movie..."
+                />
+                <Button
+                    variant="outline"
+                    className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg px-6"
+                    type="submit"
+                >
+                    Search
+                </Button>
+            </form>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
